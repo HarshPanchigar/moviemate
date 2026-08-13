10af93a9693f6fb626972ae39be82793bb730296
Harsh Panchigar <harshpanchigar@gmail.com>
2026-08-13 22:06:30 +0530

    start next phase

--- moviemate/accounts/admin.py
+++ moviemate/accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'email', 'mobile')
+    search_fields = ('full_name', 'email', 'mobile')
